aeeb416aff4a2b8c3d816ec431d8edb5e9ea2bb6
Stania Svobodova <greg.morris85@gmail.com>
2018-11-30 10:19:11 +0000

    Add admin user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     
     helper_method :current_user, :logged_in?
+    
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
@@ -11,7 +12,7 @@ class ApplicationController < ActionController::Base
     end
 
     def require_user
-        if !logged_in?
+        if !logged_in? 
             flash[:danger] = "You must be logged in to perform that action"
             redirect_to root_path
         end
